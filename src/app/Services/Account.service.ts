88d48969731b5258084b618e88faa1fb0a77b0b9
Sarvda Nand Rai <dipurai1986>
2023-05-28 20:05:52 +0530

    initial commit

---
+++ src/app/Services/Account.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  private apiUrl = 'https://localhost:7165'; // Replace with your API URL
+
+  constructor(private http: HttpClient) {}
+
+  getAccounts(userId: number): Observable<any> {
+    const url = `${this.apiUrl}/api/Account/GetAccounts/${userId}`;
+    return this.http.get<any>(url);
+  }
+
+  createAccount(userId: number) {
+    const headers = new HttpHeaders()
+
+   const url = `${this.apiUrl}/api/Account/CreateAccount`; 
+    const body = {
+      userId: userId
+    };
+
+    return this.http.post(url, body, { headers });
+  
+  }
+
+  deposit(userId: number, accountId: number, amount: number): Observable<any> {
+    const url = `${this.apiUrl}/api/Account/Deposit`;
+    const body = { userId, accountId, amount };
+    return this.http.post<any>(url, body);
+  }
+
+  withdraw(userId: number, accountId: number, amount: number): Observable<any> {
+    const url = `${this.apiUrl}/api/Account/Withdraw`;
+    const body = { userId, accountId, amount };
+    return this.http.post<any>(url, body);
+  }
+
+  deleteAccount(userId: number, accountId: number): Observable<any> {
+    const url = `${this.apiUrl}/api/Account/DeleteAccount/${userId}/${accountId}`;
+    return this.http.delete<any>(url);
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AccountService } from './Services/Account.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,130 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'account_app_ui';
+  accounts: any[] = [];
+  accountId: number = 0;
+  userAccountNo:string="";
+  amount: number = 0;
+  errorMessage: string = '';
+  successMessage:string='';
+  transactionType: string = '';
+  userid:number=100;
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+
+    this.getAccounts(this.userid); 
+
+  
+  }
+
+  getAccounts(userId: number): void {
+    this.accountService.getAccounts(userId)
+      .subscribe(
+        response => {
+          this.accounts = response;
+        },
+        error => {
+          this.errorMessage = 'Error fetching accounts.';
+        }
+      );
+  }
+
+  createAccount(): void {
+    this.accountService.createAccount(this.userid) 
+      .subscribe(
+        response => {
+        
+          this.getAccounts(this.userid); 
+        },
+        error => {
+          this.errorMessage = 'Error in creating account.';
+        }
+      );
+  }
+
+  deposit(account: any): void {
+    this.transactionType = 'Deposit';
+    this.userAccountNo=account.accountNumber
+    this.amount=0;
+    this.accountId=account.accountId
+  }
+
+  withdraw(account: any): void {
+    this.transactionType = 'Withdraw';
+
+    this.userAccountNo=account.accountNumber
+    this.accountId=account.accountId
+  }
+
+  deleteAccount(account: any): void {
+    const userId = this.userid; 
+    const accountId = account.accountId;
+    this.accountService.deleteAccount(userId, accountId)
+      .subscribe(
+        response => {
+          
+          this.getAccounts(this.userid); 
+          this.successMessage="Account Deleted Successfuly";
+        },
+        error => {
+          this.errorMessage = 'Error deleting account.';
+        }
+      );
+  }
+
+  submitTransaction(): void {
+    // Implement the logic for deposit or withdrawal based on transactionType
+    const userId = this.userid; // Pass the user ID as an argument
+    const accountId = this.accountId;
+    const amount = this.amount;
+
+    if (this.transactionType === 'Deposit') {
+      this.accountService.deposit(userId, accountId, amount)
+        .subscribe(
+          response => {
+            this.errorMessage="";
+            this.getAccounts(this.userid); 
+            this.successMessage="Deposit Successfuly";
+            this.amount=0;
+          },
+          error => {
+            this.handleError(error);
+          }
+        );
+    } else if (this.transactionType === 'Withdraw') {
+      this.accountService.withdraw(userId, accountId, amount)
+        .subscribe(
+          response => {
+            this.errorMessage="";
+            this.getAccounts(this.userid); 
+            this.successMessage="withdraw Successfuly";
+            this.amount=0;
+          },
+          error => {
+            this.handleError(error);
+           
+          }
+        );
+    }
+  }
+  private handleError(error: HttpErrorResponse) {
+    this.errorMessage = 'An error occurred';
+    this.successMessage='';
+    var errorList="";
+  
+      if (error.error && error.error.errors) {
+        errorList = error.error.errors[''];
+        if(!Array.isArray(errorList)){
+          errorList= error.error.errors.Amount;
+        }
+        if (Array.isArray(errorList) && errorList.length > 0) {
+          this.errorMessage = errorList[0];
+        }
+      }
+    
+
+   
+  }
 }
